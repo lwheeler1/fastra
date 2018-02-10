411114b2e8ee3d8968e359da9c32ab8c69de5d0d
Logan Wheeler <lwheeler1@carthage.edu>
2018-02-10 17:26:23 -0600

    MWF class schedule buttons all set up

--- public/src/app/js/modules/PracticeComp.js
+++ public/src/app/js/modules/PracticeComp.js
@@ -21,9 +21,94 @@ export class PracticeComp extends React.Component {
 			<div className="leftColumn">
 				<h1>left</ h1>
 			</div>
-			<div className="middleColumn">
-				<h1>middle</ h1>
+			<div className="dateTimes">
+				<h3 className="timeSlot">Day:</h3>
+				<h3 className="timeSlot">07:00--</h3>
+				<h3 className="timeSlot">08:00--</h3>
+				<h3 className="timeSlot">09:00--</h3>
+				<h3 className="timeSlot">10:00--</h3>
+				<h3 className="timeSlot">11:00--</h3>
+				<h3 className="timeSlot">12:00--</h3>
+				<h3 className="timeSlot">01:00--</h3>
+				<h3 className="timeSlot">02:00--</h3>
+				<h3 className="timeSlot">03:00--</h3>
+				<h3 className="timeSlot">04:00--</h3>
+				<h3 className="timeSlot">05:00--</h3>
+				<h3 className="timeSlot">06:00--</h3>
+				<h3 className="timeSlot">07:00--</h3>
+				<h3 className="timeSlot">08:00--</h3>
+				<h3 className="timeSlot">09:00--</h3>
 			</div>
+				<div className="sun">
+					<h1 className="daySlot">Sun</h1>
+
+				</div>
+				<div className="mon">
+					<h1 className="daySlot">Mon</h1>
+					<button class="onehrButton">Click to reserve!</button>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiftyminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiftyminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="threehrButton">Click to reserve!</button>
+				</div>
+				<div className="tue">
+					<h3 className="daySlot">Tue</h3>
+				</div>
+				<div className="wed">
+					<h3 className="daySlot">Wed</h3>
+					<button class="onehrButton">Click to reserve!</button>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiftyminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiftyminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="threehrButton">Click to reserve!</button>
+				</div>
+				<div className="thu">
+					<h3 className="daySlot">Thu</h3>
+				</div>
+				<div className="fri">
+					<h3 className="daySlot">Fri</h3>
+					<button class="onehrButton">Click to reserve!</button>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiftyminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="onehrfiveminButton">Click to reserve!</button>
+					<div className="fivemins"> </div>
+					<button class="onehrfiftyminButton">Click to reserve!</button>
+					<div className="tenmins"> </div>
+					<button class="threehrButton">Click to reserve!</button>
+				</div>
+				<div className="sat">
+					<h3 className="daySlot">Sat</h3>
+				</div>
 			<div className="rightColumn">
 				<h1>right</ h1>
 			</div>
